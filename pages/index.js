8fbdc1dc3fc35f345a59fe0e621a233a2a3a9c1b
OckTheOctopus <Yaseen6607@yahoo.com>
2022-09-03 14:21:51 +1000

    Link fixing

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ export default function Home() {
             <h2>Suggested clubs:</h2>
             <div className='gallery'>
               <Link href='/clubs/ChatForChange'><a><Image className="gallery-image" src={ChatForChange_logo} alt="Chat for Change" width='300px' height='300px'/></a></Link>
-              <Link href='/TechCrew'><a><Image className="gallery-image" src={TechCrew_logo} alt="Tech Crew" width='300px'height='300px'/></a></Link>
-              <Link href='/FCSC'><a><Image className="gallery-image" src={FCSC_logo} alt="Fundraising and Community Services Comittee" width='300px' height='300px'/></a></Link>
+              <Link href='/clubs/TechCrew'><a><Image className="gallery-image" src={TechCrew_logo} alt="Tech Crew" width='300px'height='300px'/></a></Link>
+              <Link href='/clubs/FCSC'><a><Image className="gallery-image" src={FCSC_logo} alt="Fundraising and Community Services Comittee" width='300px' height='300px'/></a></Link>
             </div>
           </main>
       </div>
